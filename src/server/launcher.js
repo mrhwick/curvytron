2d9143138635cb650ce9d65e793278c9ffdd04a5
Mrhwick <matt.hardwick@getbellhops.com>
2015-05-06 13:59:27 -0400

    fix

--- src/server/launcher.js
+++ src/server/launcher.js
@@ -5,7 +5,7 @@ try {
     config = require('../config.json');
 } catch (error) {
     config = {
-        port: 8080,
+        port: process.env.PORT,
         inspector: { enabled: false }
     };
 }
